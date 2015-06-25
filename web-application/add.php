3a5fe43c2e63c5e6f78534b3c1a0d3b3a259be5b
bizarredeveloper <bizarredeveloper@gmail.com>
2015-06-25 16:57:18 +0530

    test

---
+++ web-application/add.php
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51a5fa9c59636a3d"></script>
+
+<meta property="og:title" content="AddThis Tour" />
+     <meta property="og:description" content="Watch the AddThis Tour video." />
+     <meta property="og:image" content="http://i2.ytimg.com/vi/1F7DKyFt5pY/default.jpg" />          
+	 <meta property="og:video" content="http://www.youtube.com/v/1F7DKyFt5pY&fs=1" />   
+     <meta property="og:video:width" content="560" />    
+     <meta property="og:video:height" content="340" />     
+     <meta property="og:video:type" content="application/x-shockwave-flash" /> 
+<a class="addthis_button" addthis:url="http://www.addthis.com/pages/embedded-example-meta"> 
