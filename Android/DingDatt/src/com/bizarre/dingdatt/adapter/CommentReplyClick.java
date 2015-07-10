775303d79519f5f99231ec72180e39bf5f27ea77
bizarredeveloper <bizarredeveloper@gmail.com>
2015-07-10 15:00:42 +0530

    web-application

--- Android/DingDatt/src/com/bizarre/dingdatt/adapter/CommentReplyClick.java
+++
@@ -1,6 +0,0 @@
-package com.bizarre.dingdatt.adapter;
-
-public interface CommentReplyClick {
-
-	public void onReplyClick(int iPos);
-}
--- Android/DingDatt/src/com/bizarre/dingdatt/strings/JSONStrings.java
+++
@@ -1,9 +0,0 @@
-package com.bizarre.dingdatt.strings;
-
-public class JSONStrings {
-
-	public static String JSON_RESPONSE = "response";
-	public static String JSON_SUCCESS = "success";
-	public static String JSON_USERID = "userid";
-	public static String JSON_MESSAGE = "message";
-}
